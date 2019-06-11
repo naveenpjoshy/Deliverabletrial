49d94fb9913812892f419756bd24a71752b8e670
denbinjose <joseden@sheridancollege.ca>
2019-06-10 23:32:05 -0400

    Student Demo Pro

--- src/ca/sheridancollege/project/Player.java
+++ src/ca/sheridancollege/project/Player.java
@@ -7,42 +7,31 @@ package ca.sheridancollege.project;
 
 /**
  * A class that models each Player in the game. Players have an identifier, which should be unique.
- * @author dancye, 2018
+ * @author Denbin_Jose, 2019
  */
-public abstract class Player 
-{
-    private String playerID; //the unique ID for this player
-    
-    /**
-     * A constructor that allows you to set the player's unique ID
-     * @param name the unique ID to assign to this player.
-     */
-    public Player(String name)
-    {
-        playerID= name;
-    }
-    
-    /**
-     * @return the playerID
-     */
-    public String getPlayerID() 
-    {
+public class Player{
+     Card[] playerOneCards;
+     Card[] playerTwoCards;
+    private int[] value;
+    private String playerID;
+    public String getPlayerID(){
         return playerID;
     }
-
-    /**
-     * Ensure that the playerID is unique
-     * @param givenID the playerID to set
-     */
-    public void setPlayerID(String givenID) 
-    {
-        playerID = givenID;
+    public void setPlayerID(String givenID){
+    playerID=givenID;
     }
-    
-    /**
-     * The method to be instantiated when you subclass the Player class
-     * with your specific type of Player and filled in with logic to play your game.
-     */
-    public abstract void play();
-    
+
+    Player(GroupOfCards d,int gameNum)
+    {
+        playerID= "playerName";
+        value = new int[gameNum+3];
+        playerOneCards = new Card[gameNum+2];
+        playerTwoCards = new Card[gameNum+2];
+             
+        for (int a=0; a<gameNum+3; a++)
+        {
+        playerOneCards[a] = d.drawFromDeck(); 
+        playerTwoCards[a] = d.drawFromDeck(); 
+        }
+    }
 }
